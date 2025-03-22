387f9eb6d21ff53511cf96e49a30c0e7bfef10b2
Suraj Vaidyanathan <suraj.vaidyanathan@gmail.com>
2025-03-22 15:24:30 +0530

    Implemented RPS script

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-console.log("Hello World!")
+console.log("Hello World!");
+
+function getComputerChoice(){
+    let choice = Math.floor(Math.random() * 3);
+    switch(choice){
+        case 0:
+            return "rock";
+            break;
+        case 1:
+            return "paper";
+            break;
+        case 2:
+            return "scissors";
+    }
+}
+
+function getHumanChoice(){
+    while (true){
+        let choice = parseInt(prompt("Enter\n1.For Rock\n2. For Paper\n3.For Scissors"));
+        switch(choice){
+            case 1:
+                return "rock";
+            case 2:
+                return "paper";
+            case 3:
+                return "scissors";
+        }
+    }
+}
+
+function playRound(humanChoice, computerChoice){
+    let result;
+    if (humanChoice === computerChoice) result = "tied with";
+    else if ( (humanChoice === "rock" && computerChoice === "paper") 
+        || (humanChoice === "paper" && computerChoice === "scissors")
+        || (humanChoice === "scissors" && computerChoice === "rock")) result = "lost to";
+    else result = "won against";
+
+    console.log(`Your choice of ${humanChoice} ${result} ${computerChoice}`);    
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
